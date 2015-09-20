f47e431822a5afa5efd4234b335bb6ac508b0cd8
mahiki <mahiki@users.noreply.github.com>
2015-09-19 19:09:18 -0700

    still not understanding objects i guess

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ examples from the Java 8 distribution have `-java8` in the filename
 
 
 ##### phase 3 - objects
-- [ ] deduplicate array of objects (issue #9)
+- [ ] deduplicate array of objects [issue #9](https://github.com/mahiki/java-loop-probs/issues/9)
--- p3-object-array-dedupe/DogArray.java
+++ p3-object-array-dedupe/DogArray.java
@@ -1,23 +1,23 @@
 public class DogArray{
-   
-   String dogName;
-   
-   public Dog(){
-      
-      dogName = toString(StdRandom.uniform(1000));
-   }
-   
-   public DogArray[](int N){
-      
-      for(int i = 0; i < N; i++){
-         
-         DogArray[] dogs[i] = new Dog();
-         
-      }
-   }
-   
-   public static void main(String[] args){
-      
-      
-   }
+ 
+    String dogName;
+
+    public Dog(){
+
+    }
+
+    public DogArray[](int N){
+
+        for(int i = 0; i < N; i++){
+
+            DogArray[] dogs[i] = new Dog();
+            dogName = toString(StdRandom.uniform(1000));
+
+        }
+    }
+
+    public static void main(String[] args){
+    
+    
+    }
 }
